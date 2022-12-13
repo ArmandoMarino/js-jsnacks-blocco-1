f6fbcca29c5f7d177fa49701764ea7c18f57dd2b
ArmandoMarino <scrivania.lavoro@gmail.com>
2022-12-13 17:32:20 +0100

    101 for jsnack2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -4,3 +4,11 @@ console.log("JS OK");
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
 //  l’utente inserisce il suo nome tramite un input.
 //  comunicagli se può partecipare o no alla festa. vietato usare includes
+
+
+// PRENDO GLI ELEMENTI DAL DOM
+
+
+const guests = ["Armando" , "Elena" , "Emilio", "Grazia", "Alessandro"];
+
+
