8f75d6b75ba40cdda85f0e2c90c315c24cfc01b2
ArmandoMarino <scrivania.lavoro@gmail.com>
2022-12-13 20:28:31 +0100

    jsnack 3 update

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,1 +1,32 @@
 console.log("JS OK");
+
+// SNACK BONUS
+// L'utente inserisce un numero di 4 cifre in un input. 
+// Calcola la somma di tutte le cifre che compongono il numero 
+// e stampala in pagina.
+// Esempio: l'utente inserisce 1234, noi stampiamo 10.
+
+
+// PRENDO GLI ELEMENTI DAL DOM
+const number = document.getElementById("number");
+console.log(number);
+
+// BUTTON ID
+const button = document.getElementById ("button-enter");
+console.log(button);
+
+let strUser = [];
+
+
+button.addEventListener("click", function(){
+    numberUser = parseInt(number.value.trim());
+
+    console.log(numberUser + "Number User");
+    
+    for ( let i = 0; i < strUser.length; i++){
+
+    }
+
+  
+   
+});
