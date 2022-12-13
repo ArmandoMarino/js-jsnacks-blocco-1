3e67115098def4c8787299c69408215894049665
ArmandoMarino <scrivania.lavoro@gmail.com>
2022-12-13 16:42:23 +0100

    jsnack2 101

---
+++ jsnack2/script.js
@@ -0,0 +1,6 @@
+console.log("JS OK");
+
+// SNACK 2
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
+//  l’utente inserisce il suo nome tramite un input.
+//  comunicagli se può partecipare o no alla festa. vietato usare includes
